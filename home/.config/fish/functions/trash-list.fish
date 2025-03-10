e24b1169400e5e3af2764108cace9911e36b6e73
LilLongg <glivingstone70@gmail.com>
2025-03-10 19:02:39 +0700

    Add trash-list to du, add softwrap for helix, add pacages

--- home/.config/fish/functions/trash-list.fish
+++ home/.config/fish/functions/trash-list.fish
@@ -1,4 +1,4 @@
 function trash-list --wraps="nu -c 'ls ~/.local/share/Trash/files'" --description "alias trash-list=nu -c 'ls ~/.local/share/Trash/files'"
-    echo \"(string join ' ' ls ~/.local/share/Trash/files)\" | xargs nu -c
+    echo \"(string join ' ' ls ~/.local/share/Trash/files) --du\" | xargs nu -c
 
 end
