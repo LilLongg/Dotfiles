83fde670dd999f171fd9c5aee5568a8a8849985b
LilLongg <glivingstone70@gmail.com>
2025-04-27 00:58:09 +0700

    o7 conda

---
+++ home/.config/fish/functions/activate.fish
@@ -0,0 +1,4 @@
+function activate --wraps='source /home/TNonggChann/.venv//bin/activate.fish' --description 'alias activate=source /home/TNonggChann/.venv//bin/activate.fish'
+    source /home/TNonggChann/.venv/$argv/bin/activate.fish
+
+end
---
+++ home/.config/fish/functions/create.fish
@@ -0,0 +1,4 @@
+function create --wraps='uv venv --directory=/home/TNonggChann/.venv' --description 'alias create=uv venv --directory=/home/TNonggChann/.venv'
+  uv venv --directory=/home/TNonggChann/.venv $argv
+        
+end
