a6c5f49c2e13d7b92c3700ce4659624172ec768c
LilLongg <glivingstone70@gmail.com>
2025-05-29 18:48:02 +0700

    Update commands to not change directory when create new venv

--- home/.config/fish/scripts/venv.fish
+++ home/.config/fish/scripts/venv.fish
@@ -4,13 +4,18 @@ switch $argv[1]
     case list
         $HOME/.config/fish/scripts/vlist.nu
     case create
-        uv venv --directory=$HOME/.venv $argv[2..]
+        uv venv --directory=$HOME/.venv $argv[2..] --relocatable
+        set -l cur_dir (pwd)
         set -l new_venv (ls -t1 $HOME/.venv | head -n1)
         source $HOME/.venv/$new_venv/bin/activate.fish
         uv pip install -r $HOME/.config/python_defaults.txt
         deactivate
+        cd $cur_dir
     case remove
         rm -rf $HOME/.venv/$argv[2]
     case activate
         source $HOME/.venv/$argv[2]/bin/activate.fish
+    case rename
+        rm -rf $HOME/.venv/$argv[3]
+        mv $HOME/.venv/$argv[2] $HOME/.venv/$argv[3]
 end
