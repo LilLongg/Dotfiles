2eb95beb33e93f135e57bd29cc1f3207a9e0ee3c
LilLongg <glivingstone70@gmail.com>
2025-05-29 18:32:29 +0700

    Update commands to auto install packages to new venvs

--- home/.config/fish/scripts/venv.fish
+++ home/.config/fish/scripts/venv.fish
@@ -5,6 +5,10 @@ switch $argv[1]
         $HOME/.config/fish/scripts/vlist.nu
     case create
         uv venv --directory=$HOME/.venv $argv[2..]
+        set -l new_venv (ls -t1 $HOME/.venv | head -n1)
+        source $HOME/.venv/$new_venv/bin/activate.fish
+        uv pip install -r $HOME/.config/python_defaults.txt
+        deactivate
     case remove
         rm -rf $HOME/.venv/$argv[2]
     case activate
