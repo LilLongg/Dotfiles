0237c5c84ccda14ae977f7ada3117f441612bbf5
LilLongg <glivingstone70@gmail.com>
2025-07-13 02:48:08 +0700

    Update wpgtk

--- home/.config/i3/scripts/bgaction.fish
+++ home/.config/i3/scripts/bgaction.fish
@@ -10,40 +10,38 @@ ln -sf $IMG_CACHE ~/.config/hypr/asset/LOCK.JPG
 switch (string split . $IMG_PATH | tail -n1)
     case mp4 MP4
         if not test -f $LOCK_FILE
-            swww kill
             touch $LOCK_FILE
         end
         if not test -f $IMG_CACHE
             ffmpeg -i $IMG_PATH -vf "select=eq(n\,0)" -q:v 1 $IMG_CACHE
         end
-        mpvpaper -o "--loop-playlist --hwdec=vaapi" '*' $IMG_PATH >/dev/null &
+        nice xwinwrap -ov -fs -fdt -ni -- mpv -wid WID -loop $IMG_PATH &
     case gif GIF
         if test -f $LOCK_FILE
             sh -c "rm $LOCK_FILE"
-            kill mpvpaper
         end
         if not test -f $IMG_CACHE
             ffmpeg -i $IMG_PATH -vframes 1 $IMG_CACHE
         end
-        swww img --transition-type random $IMG_PATH >/dev/null
+        nice xwinwrap -ov -fs -fdt -ni -- mpv -wid WID -loop $IMG_PATH &
     case '*'
         if test -f $LOCK_FILE
             sh -c "rm $LOCK_FILE"
-            kill mpvpaper
         end
+        kill xwinwrap
         if not test -f $IMG_CACHE
-            ffmpeg -i $IMG_PATH $IMG_CACHE
+            ffmpeg $IMG_PATH $IMG_CACHE
         end
-        swww img --transition-type random $IMG_PATH >/dev/null
+        feh --no-fehbg --bg-scale $IMG_PATH
 end
 
+wpg -ns "$IMG_CACHE"
 matugen image $IMG_CACHE
 wallust run $IMG_CACHE
 
 cp ~/.cache/wallust/sequences ~/.cache/wal/sequences
 sed -i 's/""/"~\/.config\/hypr\/asset\/LOCK.JPG"/g' ~/.cache/wal/colors.json
 
-kitty @ set-colors -a -c ~/.config/kitty/colors.conf
 pywalfox update
 pkill -USR2 cava
 dunstctl reload
