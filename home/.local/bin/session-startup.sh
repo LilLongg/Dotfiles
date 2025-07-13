847c4e1e46591fc574585da8297717d8d7398e42
LilLongg <glivingstone70@gmail.com>
2025-07-13 09:21:13 +0700

    o7 qt

--- home/.local/bin/session-startup.sh
+++ home/.local/bin/session-startup.sh
@@ -5,11 +5,11 @@ switch $XDG_SESSION_TYPE
         ln -sf ~/.config/dunst/dunstrc-wayland ~/.config/dunst/dunstrc
         ln -sf ~/.config/electron-flags-wl.conf ~/.config/electron-flags.conf
         ln -sf ~/.config/code-flags-wl.conf ~/.config/code-flags.conf
-        sed -i "s/Terminal=.*/Terminal=kitty/g" ~/.config/pcmanfm-qt/default/settings.conf
+        sed -i "s/terminal=.*/terminal=kitty/g" ~/.config/libfm/libfm.conf
     case x11
         ln -sf ~/.config/dunst/dunstrc-x11 ~/.config/dunst/dunstrc
         rm ~/.config/code-flags.conf ~/.config/electron-flags.conf
-        sed -i "s/Terminal=.*/Terminal=alacritty/g" ~/.config/pcmanfm-qt/default/settings.conf
+        sed -i "s/terminal=.*/terminal=alacritty/g" ~/.config/libfm/libfm.conf
     case '*'
 end
 
